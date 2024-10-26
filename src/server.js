507e4462c0d8a5d4653bb1d3417384c52e6fe69a
정지호 <jeongjiho@jeongjihoui-MacBookAir.local>
2024-10-27 01:25:44 +0900

    https

--- src/server.js
+++ src/server.js
@@ -1,38 +1,42 @@
 import express from "express";
 import SocketIO from "socket.io";
-import http from "http";
+import https from "https";
+import fs from "fs";
 
+// Express 앱 설정
 const app = express();
-
 app.set("view engine", "pug");
-app.set("views", __dirname + "/views"); 
-app.use("/public",express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req,res) => res.redirect("/"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log('Listening on http://localhost:3000');
+// HTTPS 서버 옵션 설정
+const options = {
+  key: fs.readFileSync(__dirname +"/private.key"),
+  cert: fs.readFileSync(__dirname +"/certificate.crt"),
+};
 
-const httpServer = http.createServer(app);//create http server
-const wsServer = SocketIO(httpServer);
+// HTTPS 서버와 Socket.IO 서버 생성
+const httpsServer = https.createServer(options, app);
+const wsServer = SocketIO(httpsServer);
 
- wsServer.on("connection", socket => {
-    socket.on("join_room", (roomName) => {
-        socket.join(roomName);
-        socket.to(roomName).emit("welcome");
-    });
-    socket.on("offer", (offer, roomName) => {
-        socket.to(roomName).emit("offer", offer);
-    });
-    socket.on("answer", (answer, roomName) => {
-        socket.to(roomName).emit("answer", answer);
-    });
-    socket.on("ice", (ice, roomName) => {
-        socket.to(roomName).emit("ice", ice);
-    });
-    
- });
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
+});
 
-httpServer.listen(3000, handleListen);  
-
-
- 
+// 서버 포트 설정 및 실행
+const handleListen = () => console.log("Listening on https://localhost:3000");
+httpsServer.listen(3000, handleListen);
